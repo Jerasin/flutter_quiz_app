b7fa4939dad7d69634fe8c504d1915174879bc44
Jerasin <75404545+Jerasin@users.noreply.github.com>
2022-08-20 21:31:27 +0700

    Learning (#1)
    * learning: button
    
    * learning: add calculate scores
    
    * fix: remove commend

--- lib/result.dart
+++ lib/result.dart
@@ -6,6 +6,18 @@ class Result extends StatelessWidget {
   const Result({Key? key, required this.totalScore, required this.resetScore})
       : super(key: key);
 
+  String get resultPhrase {
+    if (totalScore == 0) {
+      return 'Failed to score';
+    } else if (totalScore > 0 && totalScore <= 50) {
+      return 'Play again';
+    } else if (totalScore > 50) {
+      return 'Congratulation';
+    } else {
+      return 'Error';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -14,7 +26,19 @@ class Result extends StatelessWidget {
         Center(
           child: Text('Score : $totalScore'),
         ),
-        ElevatedButton(onPressed: resetScore, child: const Text('New Game'))
+        const SizedBox(
+          height: 30,
+        ),
+        Center(
+          child: Text(
+            resultPhrase,
+            style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+        ElevatedButton(onPressed: resetScore, child: const Text('New Game')),
       ],
     );
   }
